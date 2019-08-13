cff9695a05ccdace47615f577a102acb3fe66642
nick chen <nickchen.dev@gmail.com>
2019-08-13 16:21:34 +1000

    change 35.c

--- w3/task3/35.c
+++ w3/task3/35.c
@@ -16,22 +16,18 @@ int main()
     name1.firstName = "first1";
     name1.lastName = "last1";
 
-    name2.firstName = "first2";
-    name2.lastName = "last2";
+    name2.firstName = "first1";
+    name2.lastName = "last1";
 
     int result = compareName(name1, name2);
     char *output;
-    if (result < 0)
-    {
-        output = "name1 is less than name2\n";
-    }
-    else if (result == 0)
+    if (result == 0)
     {
         output = "name1 is equal to name2\n";
     }
     else
     {
-        output = "name1 is greater than name2\n";
+        output = "name1 is not equal to name2\n";
     }
 
     printf("result = %s\n", output);
@@ -41,7 +37,15 @@ int main()
 int compareName(Name name1, Name name2)
 {
     int result;
-    result = strcmp(name1.firstName, name2.firstName) +
-             strcmp(name1.lastName, name2.lastName);
+    // not equal
+    if (strcmp(name1.firstName, name2.firstName) ||
+        strcmp(name1.lastName, name2.lastName))
+    {
+        result = 1;
+    } else {
+        // firstName and lastName both equal
+        result = 0;
+    }
+
     return result;
 }
